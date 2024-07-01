2974c93092db837356d5f4cc62fd35422cc59798
christiangerbig <chr_gerbig@web.de>
2024-07-01 16:34:43 +0200

    update

--- exe-file-handler.asm
+++ exe-file-handler.asm
@@ -1,5 +1,6 @@
-; Load/start/remove a demo as an exe file
-; Important: dos.library is expected as opened and its base is already stored
+; Manually load/run/unload a demo as an exe file with the dos.library
+; Important: The dos.library is expected as opened and its base is already stored
+;	     No cleanup code for the dos.library included
 
 exec_base			EQU $0004
 LIB_VERSION                     EQU 20	;Offset in library base
@@ -17,7 +18,10 @@ RETURN_FAIL			EQU 20
 ASCII_LINE_FEED			EQU 10
 
 
-start
+; ** Other code **
+
+	CNOP 0,4
+load_demo
 	lea     demofile_path(pc),a0
 	move.l  a0,d1
 	move.l  dos_base(pc),a6
@@ -31,26 +35,24 @@ start
 check_os_version
 	move.l  exec_base.w,a6
 	cmp.w   #OS2_VERSION,LIB_VERSION(a6) ;OS2.04 or better
-	blo.s	load_demo
+	blo.s	run_demo
 	jsr	_LVOCacheClearU(a6)	;Mandatory on 680x0 systems
+run_demo
 
-load_demo
+; ** Some demos expect these values in d0/a0 or there will be a guru meditation **
+	lea	shell_command_line(pc),a0 ;Pointer to pseudo command line string
+	moveq	#shell_command_line_end-shell_command_line,d0 ;Length = 1 character
+
 	move.l	demofile_seglist(pc),a3 ;Pointer to SegList structure in a3 is not mandatory, but some demos expect it
 	add.l	a3,a3			;Get BCPL pointer
 	add.l	a3,a3
-
-; ** Some demos expect these values in d0/a0 or there will be a guru meditation **
-	lea	shell_command_line(pc),a0 ;Pointer to line feed command line string
-	moveq	#shell_command_line_end-shell_command_line,d0 ;Length = 1 character
-
  	movem.l d2-d7/a2-a6,-(a7)
 	jsr	FirstCode(a3)		;Execute demo
         movem.l (a7)+,d2-d7/a2-a6
-
 unload_demo
 	move.l	demofile_seglist(pc),d1
 	move.l	dos_base(pc),a6
-	jsr	_LVOUnLoadSeg
+	jsr	_LVOUnLoadSeg(a6)
 	moveq	#RETURN_OK,d0
 exit
 	rts
@@ -64,7 +66,6 @@ demofile_seglist		DC.L 0
 demofile_path			DC.B "exampledemo.exe",0
 	EVEN
 shell_command_line		DC.B ASCII_LINE_FEED ;pseudo Shell commandline
-shell_command_line_end
-	DC.B 0
+shell_command_line_end		DC.B 0
 
 	END
