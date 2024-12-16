76b586917bac8238f6eeb412c32777360d2a76f8
christiangerbig <chr_gerbig@web.de>
2024-12-16 18:24:20 +0100

    update

--- exe-file-handler.asm
+++ exe-file-handler.asm
@@ -3,22 +3,21 @@
 ;	     No cleanup code for the dos.library included
 
 exec_base			EQU $0004
-LIB_VERSION                     EQU 20	;Offset in library base
+LIB_VERSION                     EQU 20
 _LVOCacheClearU			EQU -636
-OS2_VERSION                     EQU 37	;OS2.04
 
-FirstCode			EQU 4	;Offset in SegList structure
+FirstCode			EQU 4	; offset in SegList structure
 _LVOLoadSeg			EQU -150
 _LVOUnloadSeg			EQU -156
 
-; ** AmigaDOS command return codes **
+; AmigaDOS command return codes
 RETURN_OK			EQU 0
 RETURN_FAIL			EQU 20
 
 ASCII_LINE_FEED			EQU 10
 
 
-; ** Other code **
+; other code
 
 	CNOP 0,4
 load_demo
@@ -28,28 +27,22 @@ load_demo
 	jsr     _LVOLoadSeg(a6)
 	lea	demofile_seglist(pc),a0
 	move.l  d0,(a0)
-	bne.s   check_os_version
+	bne.s   run_demo
 	moveq	#RETURN_FAIL,d0
 	bra.s	exit
 	CNOP 0,4
-check_os_version
-	move.l  exec_base.w,a6
-	cmp.w   #OS2_VERSION,LIB_VERSION(a6) ;OS2.04 or better
-	blo.s	run_demo
-	jsr	_LVOCacheClearU(a6)	;Mandatory on 680x0 systems
 run_demo
 
-; ** Some demos expect these values in d0/a0 or there will be a guru meditation **
-	lea	shell_command_line(pc),a0 ;Pointer to pseudo command line string
-	moveq	#shell_command_line_end-shell_command_line,d0 ;Length = 1 character
+; Some demos expect these values in d0/a0 or there will be a guru meditation
+	lea	shell_nop_command_line(pc),a0 ; pointer pseudo command line string
+	moveq	#shell_nop_command_line_end-shell_nop_command_line,d0 ; length = 1 character
 
-	move.l	demofile_seglist(pc),a3 ;Pointer to SegList structure in a3 is not mandatory, but some demos expect it
-	add.l	a3,a3			;Get BCPL pointer
+	move.l	demofile_seglist(pc),a3 ; pointer SegList structure in a3 is not mandatory, but some demos expect it
+	add.l	a3,a3			; BCPL pointer
 	add.l	a3,a3
  	movem.l d2-d7/a2-a6,-(a7)
-	jsr	FirstCode(a3)		;Execute demo
+	jsr	FirstCode(a3)		; execute demo
         movem.l (a7)+,d2-d7/a2-a6
-unload_demo
 	move.l	demofile_seglist(pc),d1
 	move.l	dos_base(pc),a6
 	jsr	_LVOUnLoadSeg(a6)
@@ -61,11 +54,11 @@ exit
 ; ** Variables **
 	CNOP 0,4
 dos_base			DC.L 0
-
 demofile_seglist		DC.L 0
+
 demofile_path			DC.B "exampledemo.exe",0
 	EVEN
-shell_command_line		DC.B ASCII_LINE_FEED ;pseudo Shell commandline
-shell_command_line_end		DC.B 0
+shell_nop_command_line		DC.B ASCII_LINE_FEED
+shell_nop_command_line_end	DC.B 0
 
 	END
